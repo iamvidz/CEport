44c714826cdb77cdbb03374bfdde6f12c3979b90
Vidhi Maheriya <51539680+iamvidz@users.noreply.github.com>
2020-02-29 17:20:56 +0530

    login signup

---
+++ CEportal/studentlogin/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def studentlogin(request):
+    
+    return render(request,'studentlogin/slide.html')
